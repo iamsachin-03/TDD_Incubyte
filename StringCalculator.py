74aa496b72398384c5c1a72595293440cc7b303d
iamsachin-03 <47569459+iamsachin-03@users.noreply.github.com>
2022-03-06 10:54:39 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -2,8 +2,12 @@ Class Solution():
   def Test(self):
     assert self.Add("")==0, "Wrong Output"
     assert self.Add("   ")==0, "Wrong Output"
+    assert self.Add("23456")==23456, "Wrong Output"
     print("All Test Cases Passed")
   def Add(self, numbers):
-    # case1 : empty string with length
+    # case1 : empty string with length >1
     if numbers == len(numbers)*" ":
       return 0
+    # case 2: non empty string
+    elif len(numbers)>=1 and "," not in numbers and "\n" not in numbers:
+      return int(numbers)
