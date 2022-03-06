ec2c2ddaed6ad12fa4e0a8ceca9249a8de7e188a
iamsachin-03 <47569459+iamsachin-03@users.noreply.github.com>
2022-03-06 11:03:40 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -3,11 +3,21 @@ Class Solution():
     assert self.Add("")==0, "Wrong Output"
     assert self.Add("   ")==0, "Wrong Output"
     assert self.Add("23456")==23456, "Wrong Output"
+    assert self.Add("\n100\n  22\n25  ") == 147, "Wrong Output"
     print("All Test Cases Passed")
   def Add(self, numbers):
     # case1 : empty string with length >1
     if numbers == len(numbers)*" ":
       return 0
-    # case 2: non empty string
+    # case 2: one number without \n and ,
     elif len(numbers)>=1 and "," not in numbers and "\n" not in numbers:
       return int(numbers)
+    # case 3: numbers separated by \n and without having any delimiter
+    elif len(numbers)>1 and "," not in numbers and "\n" in numbers and numbers[:2] != "//":
+      number = numbers.split("\n")
+      sum = 0
+      for num in number:
+        if num != len(num)*" ":
+          sum += int(num)
+      return sum
+    
