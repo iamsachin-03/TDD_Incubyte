12577877e83ac82ed0ca1d48031cb477c388d4a2
iamsachin-03 <47569459+iamsachin-03@users.noreply.github.com>
2022-03-06 10:47:27 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,9 +1,9 @@
 Class Solution():
   def Test(self):
-    assert Add("")==0, "Doesn't return 0"
-    assert Add("   ")==0, "Doesn't return 0"
+    assert self.Add("")==0, "Wrong Output"
+    assert self.Add("   ")==0, "Wrong Output"
     print("All Test Cases Passed")
   def Add(self, numbers):
     # case1 : empty string with length
-    if len(numbers)==0:
+    if numbers == len(numbers)*" ":
       return 0
